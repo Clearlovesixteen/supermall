da3717db95bf909d1ec62a875e662e8f86dda3b1
Youyi <1326528817@qq.com>
2020-08-26 14:41:44 +0800

    轮播图

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,31 +1,39 @@
 <template>
   <div id="home">
     <nav-bar class="home-nav"> 
-      <div slot="center"><p>购物街</p></div>
+      <div slot="center">
+        <p>购物街</p>
+      </div>
     </nav-bar>
+    <home-swiper :banners = banners ></home-swiper>
   </div>
 </template>
 <script>
 import NavBar from 'components/common/navBar/NavBar.vue';
 import {getHomeMultidata} from 'network/home.js';
+import homeSwiper from './childComps/HomeSwiper.vue'
+
 
 export default {
   name: 'Home',
   components: {
     NavBar,
-  },
-  created() { 
-    getHomeMultidata().then(res => {
-      this.banners = res.data.banner
-      this.recommends = res.data.recommend
-    })
+    homeSwiper
   },
   data(){
     return {
       banners: [],
       recommends: []
     }
-  }
+  },
+  created() { 
+    getHomeMultidata().then(res => {
+      console.log(res)
+      this.banners = res.data.banner.list
+      this.recommends = res.data.recommend.list
+    })
+  },
+  
 }
 
 </script>
---
+++ src/views/home/childComps/HomeSwiper.vue
@@ -0,0 +1,33 @@
+<template>
+  <swiper>
+      <swiper-item v-for=" (item , idx) of banners" :key = idx>
+        <a :href="item.link">
+          <img :src="item.image" >
+        </a>
+      </swiper-item>
+  </swiper>
+</template>
+
+<script>
+import {Swiper , SwiperItem} from 'components/common/swiper/index.js'
+export default {
+  name: 'HomeSwiper',
+  components:{
+    Swiper,
+    SwiperItem
+  },
+  props:{
+    banners : {
+      type: Array,
+      default() {
+        return []
+      }
+    }
+  },
+  
+}
+</script>
+
+<style scoped>
+
+</style>
